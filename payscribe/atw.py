dddc3e2b4e95a31063e1a5e5ce8b740b0032e268
Praise Jah <olioxxayo@gmail.com>
2023-02-23 17:44:11 +0100

    Update atw.py

--- payscribe/atw.py
+++ payscribe/atw.py
@@ -1,1 +1,10 @@
 
+from payscribe import Payscribe
+
+class AirtimeToWallet(Payscribe):
+	
+	def airtimeToWallet(self):
+		return self.sender('airtime_to_wallet', {})
+		
+	def atwProcess(self, network, amount, phone, sender):
+		return self.sender('airtime_to_wallet/vend', {'network': network, 'amount':amount,'phone_number':phone, 'from':sender})
