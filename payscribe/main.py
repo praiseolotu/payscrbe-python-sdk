66f85b478508cfbbc49149b2c09b75f53bc1f5df
Praise Jah <olioxxayo@gmail.com>
2023-02-24 00:30:34 +0100

    Update main.py

--- payscribe/main.py
+++ payscribe/main.py
@@ -1,1 +1,9 @@
 
+from data import Data as dt
+from recharge import Recharge as rg
+# sample usage
+rg1 = rg('ps_live_355', 'sandbox')
+dt1 = dt('ps_live_355', 'sandbox')
+print(dt1.dataLookup('mtn'))
+print(dt1.vendData('monthly', '07083023292', 'airtel'))
+print(rg1.getCardPins('1467gf6'))
